db6c6c3027ae982b6e5a22111e04d89699303a43
unknown <nadi.codes@gmail.com>
2025-08-07 15:59:19 +0330

    feat: create home page and responsive

--- src/pages/home/home.style.ts
+++ src/pages/home/home.style.ts
@@ -9,9 +9,10 @@ export const HomeContainer = styled.section(
             display: flex;
             align-items: flex-start;
             justify-content: space-between;
-            padding-right: 48px;
-            padding-left: 48px;
+            padding-right: 40px;
+            padding-left: 40px;
             margin-bottom: 130px;
+            gap: 60px;
 
             .left-side {
                 color: #ed6560;
@@ -47,7 +48,7 @@ export const HomeContainer = styled.section(
                 }
 
                 a {
-                    width: 338px;
+                    width: 388px;
                     display: flex;
                     align-items: center;
                     justify-content: center;
@@ -70,6 +71,377 @@ export const HomeContainer = styled.section(
             img {
                 width: 835px;
                 height: 339px;
+                margin-bottom: 8px;
+            }
+
+            .label {
+                width: 835px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                color: #fff;
+            }
+        }
+
+        .cv-project {
+            min-height: 100vh;
+            margin-top: 70px;
+            border: 1px solid #363636;
+            width: 100%;
+            border-bottom: none;
+            border-top: none;
+            position: relative;
+
+            .footer {
+                color: #fff;
+                font-size: 20px;
+                font-weight: 300;
+                text-align: center;
+                height: 440px;
+                border-left: 1px solid #363636;
+                border-right: 1px solid #363636;
+                margin: 0px 40px;
+                padding-bottom: 72px;
+                display: flex;
+                align-items: flex-end;
+                justify-content: center;
+                border-top: none;
+            }
+        }
+
+        .project-list {
+            display: grid;
+            gap: 40px;
+            grid-template-columns: repeat(4, 1fr);
+            padding: 0px 40px 0px;
+
+            .row-line {
+                &::before {
+                    content: '';
+                    width: calc(100% + 40px);
+                    height: 1px;
+                    background-color: #363636;
+                    position: absolute;
+                    right: -40px;
+                    bottom: 0px;
+                }
+                &::after {
+                    content: '';
+                    width: calc(100% + 40px);
+                    height: 1px;
+                    background-color: #363636;
+                    position: absolute;
+                    right: -40px;
+                    top: 0px;
+                }
+            }
+
+            .row-line2 {
+                &::before {
+                    content: '';
+                    width: calc(100% + 40px);
+                    height: 1px;
+                    background-color: #363636;
+                    position: absolute;
+                    right: -40px;
+                    top: 40px;
+                }
+            }
+
+            .item {
+                position: relative;
+                height: max-content;
+                width: 100%;
+                height: 280px;
+
+                &:first-child,
+                &:nth-child(5) {
+                    .row-line {
+                        &::before {
+                            width: calc(100% + 80px);
+                        }
+                        &::after {
+                            width: calc(100% + 80px);
+                        }
+                    }
+
+                    .row-line2 {
+                        &::before {
+                            width: calc(100% + 80px);
+                        }
+                    }
+                }
+
+                &::before {
+                    content: '';
+                    width: 1px;
+                    height: 40px;
+                    background-color: #363636;
+                    position: absolute;
+                    right: 0;
+                    bottom: -40px;
+                }
+
+                &::after {
+                    content: '';
+                    width: 1px;
+                    height: 40px;
+                    background-color: #363636;
+                    position: absolute;
+                    left: 0;
+                    bottom: -40px;
+                }
+
+                &:nth-last-child(-n + 4) {
+                    &::before,
+                    &::after {
+                        display: none;
+                    }
+                }
+            }
+
+            .title {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 12px;
+                border: 1px solid #363636;
+                border-top: none;
+                border-bottom: none;
+                max-height: 40px;
+                min-height: 40px;
+
+                span {
+                    font-weight: 400;
+                    font-size: 16px;
+                    color: #ffffff;
+                }
+
+                small {
+                    color: #7f7f7f;
+                    font-weight: 400;
+                    font-size: 16px;
+                }
+            }
+
+            img {
+                width: 100%;
+                height: 240px;
+                object-fit: cover;
+                border-left: 1px solid #363636;
+                border-right: 1px solid #363636;
+            }
+        }
+
+        @media (max-width: 1300px) {
+            .border-line-responsive {
+                display: block;
+                width: 100%;
+                height: 1px;
+                color: red;
+                position: absolute;
+                top: 50%;
+                z-index: 1000;
+            }
+            .project-list {
+                grid-template-columns: repeat(2, 1fr);
+
+                .item {
+                    &:nth-child(odd) {
+                        .row-line {
+                            &::before {
+                                width: calc(100% + 80px);
+                            }
+                            &::after {
+                                width: calc(100% + 80px);
+                            }
+                        }
+
+                        .row-line2 {
+                            &::before {
+                                width: calc(100% + 80px);
+                            }
+                        }
+                    }
+
+                    &:nth-last-child(-n + 4) {
+                        &::before,
+                        &::after {
+                            display: block;
+                        }
+                    }
+
+                    &:nth-last-child(-n + 2) {
+                        &::before,
+                        &::after {
+                            display: none;
+                        }
+                    }
+                }
+            }
+        }
+
+        @media (max-width: 998px) {
+            padding-top: 80px;
+
+            header {
+                .right-side {
+                    width: 460px;
+
+                    a {
+                        width: 388px;
+                    }
+                }
+            }
+
+            .header-img {
+                width: 100%;
+                padding: 0px 40px;
+
+                img {
+                    width: 100%;
+                    height: 339px;
+                    margin-bottom: 8px;
+                }
+
+                .label {
+                    width: 100%;
+
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    color: #fff;
+                }
+            }
+
+            .cv-project {
+                .footer {
+                    margin: 0px;
+                    border: none;
+                }
+            }
+        }
+
+        @media (max-width: 800px) {
+            header {
+                .right-side {
+                    a {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+
+        @media (max-width: 680px) {
+            padding-top: 48px;
+
+            .project-list {
+                grid-template-columns: repeat(1, 1fr);
+
+                .item {
+                    .row-line {
+                        &::before {
+                            width: calc(100% + 80px);
+                        }
+                        &::after {
+                            width: calc(100% + 80px);
+                        }
+                    }
+
+                    .row-line2 {
+                        &::before {
+                            width: calc(100% + 80px);
+                        }
+                    }
+
+                    &:nth-last-child(-n + 2) {
+                        &::before,
+                        &::after {
+                            display: block;
+                        }
+                    }
+
+                    &:last-child {
+                        &::before,
+                        &::after {
+                            display: none;
+                        }
+                    }
+                }
+            }
+
+            header {
+                flex-direction: column;
+                padding-right: 20px;
+                padding-left: 20px;
+                margin-bottom: 28px;
+                gap: 28px;
+
+                .right-side {
+                    width: 100%;
+                }
+            }
+
+            .header-img {
+                padding: 0px 20px;
+            }
+        }
+
+        @media (max-width: 450px) {
+            .project-list {
+                padding: 0;
+
+                .item {
+                    .row-line {
+                        &::before {
+                            width: 100%;
+                            right: 0;
+                        }
+                        &::after {
+                            width: 100%;
+                            right: 0;
+                        }
+                    }
+
+                    .row-line2 {
+                        &::before {
+                            width: 100%;
+                            right: 0;
+                        }
+                    }
+                    &::before,
+                    &::after {
+                        display: none;
+                    }
+                }
+            }
+
+            .header-img {
+                padding: 0px 20px;
+                flex-direction: row;
+
+                img {
+                    width: 242px;
+                    height: 302px;
+                    margin-bottom: 0px;
+                }
+
+                .label {
+                    width: 100%;
+                    flex-direction: column;
+
+                    span {
+                        order: 1;
+                    }
+
+                    button {
+                        order: 3;
+
+                        svg {
+                            margin-top: 16px;
+                        }
+                    }
+                }
             }
         }
     `
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,11 @@
+import { Arrow } from '@/assets/icons';
 import { HomeContainer } from './home.style';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const matches = useMediaQuery('(max-width:600px)');
+
     return (
         <HomeContainer>
             <header>
@@ -17,7 +22,107 @@ const Home = () => {
                 </div>
             </header>
             <div className='header-img'>
-                <img src='/portfo5-min.webp' alt='' />
+                <img src={matches ? '/Res Landing Image.png' : '/portfo5-min.webp'} alt='' />
+                <div className='label'>
+                    <span>Projects</span>
+                    <button>
+                        <Arrow />
+                    </button>
+                    <span>پروژه‌ها</span>
+                </div>
+            </div>
+            <div className='cv-project'>
+                <div className='project-list'>
+                    <Link to='/BRAINFROG' className='item '>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>BRAINFROG</span>
+                                    <small>Application</small>
+                                </p>
+                                <img src='/portfo1-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/RAKIZ-STUDIO' className='item '>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>RAKIZ STUDIO</span>
+                                    <small>Branding, Website</small>
+                                </p>
+                                <img src='/portfo2-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/JRI' className='item '>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title '>
+                                    <span>JRI</span>
+                                    <small>Website</small>
+                                </p>
+                                <img src='/portfo1-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/TDEX' className='item '>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>TDEX</span>
+                                    <small>Application, Website</small>
+                                </p>
+                                <img src='/portfo4-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/A-EYES' className='item'>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>A-EYES</span>
+                                    <small>Application</small>
+                                </p>
+                                <img src='/portfo6-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/QUBY' className='item'>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>QUBY</span>
+                                    <small>Application, Branding</small>
+                                </p>
+                                <img src='/portfo3-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/FIRE-DRILL' className='item '>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>FIRE DRILL</span>
+                                    <small>Application</small>
+                                </p>
+                                <img src='/portfo4-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                    <Link to='/DATIS-INTERNATIONAL' className='item '>
+                        <div className='row-line'>
+                            <div className='row-line2'>
+                                <p className='title'>
+                                    <span>DATIS INTERNATIONAL</span>
+                                    <small>Website</small>
+                                </p>
+                                <img src='/portfo7-min.webp' alt='project logo' />
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+                <p className='footer'>Danial Sharifi’s Portfolio 2025</p>
             </div>
         </HomeContainer>
     );
